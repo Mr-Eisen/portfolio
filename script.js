29f73eb779a6517bb0288b3ecfc0d001cf9ecc90
Eisen <l1.5@icloud.com>
2024-05-08 14:02:10 +0300

    update

---
+++ script.js
@@ -0,0 +1,32 @@
+new TypeIt("#banner-text1", {
+    strings: "SENIOR",
+    speed: 100,
+}).go();
+setTimeout(() => {
+    new TypeIt("#banner-text2", {
+        strings: "VISUAL",
+        speed: 55,
+    }).go();
+
+}, 800);
+setTimeout(() => {
+    new TypeIt("#banner-text3", {
+        strings: "DESIGNER",
+        speed: 70,
+    }).go();
+
+}, 1600);
+setTimeout(() => {
+    new TypeIt("#banner-text4", {
+        strings: "Olga bannova",
+        speed: 55,
+    }).go();
+
+}, 2400);
+setTimeout(() => {
+    new TypeIt("#banner-text5", {
+        strings: "10+ years of experience",
+        speed: 50,
+    }).go();
+
+}, 3200);
