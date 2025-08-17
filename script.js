23ed783416a3635784732f8362f10f7c1b99d7c7
Eisen <l1.5@icloud.com>
2025-08-18 00:26:05 +0300

    Front Update

--- script.js
+++ script.js
@@ -34,3 +34,17 @@ setTimeout(() => {
     }).go();
 
 }, 3200);
+
+
+const inViewport = (entries, observer) => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+    });
+};
+
+const Obs = new IntersectionObserver(inViewport);
+const obsOptions = {};
+
+document.querySelectorAll('.design-experience').forEach(el => {
+    Obs.observe(el, obsOptions);
+});
